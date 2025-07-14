ff8b75ba802b2df1eeec9416474995d11fa8b44f
poseitutu10 <phinehasoseitutu1@gmail.com>
2025-07-14 15:10:48 +0000

    milestone 5

--- pages/property/[id].tsx
+++ pages/property/[id].tsx
@@ -3,22 +3,45 @@ import PropertyDetails from "@/components/property/PropertyDetails";
 import ReviewSection from "@/components/property/ReviewSection";
 import { PROPERTYLISTINGSAMPLE } from "@/constants";
 import { PropertyProps } from "@/interfaces";
+import axios from "axios";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const PropertyPage: React.FC = () => {
   const router = useRouter();
   const { id } = router.query;
-  const property = PROPERTYLISTINGSAMPLE.find((data) => data.name === id);
+  const [property, setProperty] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // const property = PROPERTYLISTINGSAMPLE.find((data) => data.name === id);
+
+  useEffect(() => {
+    const fetchProperty = async () => {
+      if (!id) return;
+      try {
+        const response = await axios.get(`/api/properties/${id}`);
+        setProperty(response.data);
+      } catch (error) {
+        console.error("Error fetching property details:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProperty();
+  }, [id]);
 
   console.log(property);
+
+  if(loading){
+    return <p>Loading...</p>
+  }
 
   if (!property) return <p>Property not found</p>;
 
   return (
     <div>
       <PropertyDetails property={property} />
-      <BookingSection price={property.price} />
+      <BookingSection price={property} />
       <ReviewSection reviews={[]} />
     </div>
   );
